19a094c3e4b14efd05e38820e9f0c86a1e28631c
Nina Miolane <nmiolane@gmail.com>
2023-01-11 08:10:54 -0800

    Add init code

---
+++ tests/test_basic.py
@@ -0,0 +1,20 @@
+"""Unit tests for the basic shape features."""
+
+import dyn.features.basic as basic
+import geomstats.backend as gs
+
+
+def test_perimeter():
+    """Test that the perimeter of a rectangle gives expected result."""
+    rectangle = gs.array([[2, 1], [-2, 1], [-2, -1], [2, -1]])
+    result = basic.perimeter(rectangle)
+    expected = 12
+    assert result == expected
+
+
+def test_area():
+    """Test that the area of a rectangle gives expected result."""
+    rectangle = gs.array([[2, 1], [-2, 1], [-2, -1], [2, -1]])
+    result = basic.area(rectangle)
+    expected = 8
+    assert result == expected
---
+++ tests/test_experimental.py
@@ -0,0 +1,30 @@
+"""Test functions in the experimental module."""
+
+import dyn.datasets.experimental as experimental
+import dyn.features.basic as basic
+import numpy as np
+
+
+def test_load_treated_osteosarcoma_cells():
+    """Test that function load_treated_osteosarcoma_cells runs.
+
+    And outputs correct shape.
+    """
+    n_sampling_points = 10
+    n_cells = 5
+
+    (
+        cells,
+        cell_shapes,
+        lines,
+        treatments,
+    ) = experimental.load_treated_osteosarcoma_cells(
+        n_cells=n_cells,
+        n_sampling_points=n_sampling_points,
+        quotient=["scaling", "rotation"],
+    )
+    assert cells.shape == (n_cells, n_sampling_points, 2), cells.shape
+    assert len(lines) == n_cells, len(lines)
+    assert len(treatments) == n_cells, len(treatments)
+    for cell in cell_shapes:
+        assert np.allclose(basic.perimeter(cell), 1.0), basic.perimeter(cell)
