046bcbd5fd17902e49056fd38eb98e073963e60b
Amil <amil@ucsb.edu>
2023-05-12 05:16:11 -0700

    :sparkles: New opening page, still needs work

--- cells/streamlit/cells/Hello.py
+++ cells/streamlit/cells/Hello.py
@@ -15,6 +15,8 @@ st.sidebar.success("Select a demo above.")
 st.markdown(
     """
     Geomstats is an open-source Python package for computations, statistics, and machine learning on nonlinear manifolds. Data from many application fields are elements of manifolds. For instance, the manifold of 3D rotations SO(3) naturally appears when performing statistical learning on articulated objects like the human spine or robotics arms. 
-    **👈 Select a demo from the sidebar** 
+    **
+    
+    👈 Select a demo from the sidebar** 
 """
 )
