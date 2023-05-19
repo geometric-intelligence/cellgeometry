001a34fd6ac87fc520c37d26abe6b79d41fb5c20
Amil <amil@ucsb.edu>
2023-05-19 03:40:37 -0700

    :sparkles: Added new page for PCA, updated landing page, plots now using plotly

--- cells/streamlit/cellgeometry/Hello.py
+++ cells/streamlit/cellgeometry/Hello.py
@@ -4,19 +4,57 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Welcome",
-    page_icon="👋",
+    page_title="Welcome to Cell Shape Analysis",
+    page_icon=":rocket:",
 )
 
-st.write("# Welcome to the Cell Shape Analysis App! 👋")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Geomstats is an open-source Python package for computations, statistics, and machine learning on nonlinear manifolds. Data from many application fields are elements of manifolds. For instance, the manifold of 3D rotations SO(3) naturally appears when performing statistical learning on articulated objects like the human spine or robotics arms. 
-    **
+    ![Asset 2](https://github.com/bioshape-lab/cells/assets/22850980/cf1ca7b1-b9d2-4055-b80d-e2bef7a2e796)
     
-    👈 Select a demo from the sidebar** 
+    
+""")
+
+import webbrowser
+
+# Define the GitHub link
+github_link = 'https://github.com/bioshape-lab/cells/tree/main/cells/streamlit'
+
+# Create a button
+if st.button('Go to GitHub'):
+    # Open the GitHub link in a new browser tab
+    webbrowser.open_new_tab(github_link)
+
+st.markdown("""
+## 📝 Project Description 
+
+This project focuses on the analysis and comparison of biological cell shapes using elastic metrics implemented in Geomstats. The shapes of biological cells are determined by various processes and biophysical forces, which play a crucial role in cellular functions. By utilizing quantitative measures that reflect cellular morphology, this project aims to leverage large-scale biological cell image data for morphological studies.
+
+The analysis of cell shapes has significant applications in various domains. One notable application is the accurate classification and discrimination of cancer cell lines treated with different drugs. Measures of irregularity and spreading of cells can provide valuable insights for understanding the effects of drug treatments.
+
+## 🎯 Features
+
+- Quantitative analysis and comparison of biological cell shapes using Geomstats.
+- Utilization of elastic metrics implemented in Geomstats for shape analysis.
+- Calculation of measures reflecting cellular morphology, facilitating in-depth characterization of cell shapes.
+- Leveraging large-scale biological cell image data for comprehensive morphological studies.
+- Framework for optimal matching, deforming, and comparing cell shapes using geodesics and geodesic distances.
+- Visualization of cellular shape variations, aiding in the interpretation and communication of analysis results.
+- User-friendly Streamlit app interface for seamless analysis, visualization, and interaction with biological cell data.
+- Comprehensive set of tools and methods, empowering researchers and scientists in cellular biology to gain insights and make discoveries.
+
+## 👈 Get Started
+    
+Select a demo from the sidebar to start analyzing your cell data!
+
+
+## 📄 License 
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+The MIT License is a permissive open-source license that allows you to use, modify, and distribute the code in both commercial and non-commercial projects. It provides you with the freedom to adapt the software to your needs, while also offering some protection against liability. It is one of the most commonly used licenses in the open-source community.
 """
 )
