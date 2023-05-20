510ddac69684824a512be7f7b089f0220ac649d0
SamFeinstein <66330682+SamFeinstein@users.noreply.github.com>
2023-05-20 07:17:09 -0700

    Added background information

--- cells/planks.py
+++ cells/planks.py
@@ -88,3 +88,133 @@ def exhaustive_align(curve, base_curve):
         point=gs.array(reparametrized_min), base_point=base_curve
     )
     return aligned_curve
+
+def build_rois(path: str) -> dict:
+    """
+    build_rois(path: str)
+    
+    Given an ROI filepath, extract all the ROIs.
+    
+    Parameters
+    ----------
+    path : str
+        Filepath to ROIs.
+        
+    
+    Returns
+    -------
+    out : dict
+        A dictionary of all ROIs in a filepath.
+    """
+    rois = {}
+    for roi in sorted(os.listdir(path)):
+        # print(roi.split(".")[0])
+        rois[roi.split(".")[0]] = read_roi_zip(os.path.join(path,roi))
+    return rois
+
+def dictPath(ogPath: str, path: str, dictionary: dict, sep: str="/"):
+    """
+    dictPath(ogPath: str, path: str, dictionary: dict, sep: str="/")
+    
+    Given an ROI filepath, extract all the ROIs within that folder.
+    
+    Parameters
+    ----------
+    ogPath : str
+        Original file path for data folder.
+    path : str
+        Needed for recursive call, path is first set as ogPath.
+    dictionary: dict
+        Dictionary, can be empty or an existing one to add new data to.  
+    sep: str
+        Seperator for the filepath, most likely will be "/" which is the default.
+    
+    Returns
+    -------
+    out : dict
+        A dictionary of all ROIs in a filepath.
+        
+    """
+    while path.startswith(sep):
+        path = path[1:]
+        # print(path)
+    parts = path.split(sep, 1)
+    if len(parts) > 1:
+        branch = dictionary.setdefault(parts[0], {})
+        dictPath(ogPath, parts[1], branch, sep)
+        if parts[0] == "ROIs":
+            dictionary[parts[0]] = build_rois(ogPath)
+
+def find_key(dictionary, target_key):
+    """
+    Recursively searches for a key in a nested dictionary.
+
+    Parameters:
+        dictionary (dict): The nested dictionary to search.
+        target_key (str): The key to find.
+
+    Returns:
+        object: The value associated with the target key, or None if the key is not found.
+
+    Example:
+        >>> data = {
+        ...     'key1': {
+        ...         'key2': {
+        ...             'key3': 'value3',
+        ...             'key4': 'value4'
+        ...         }
+        ...     }
+        ... }
+        >>> result = find_key(data, 'key4')
+        >>> print(result)
+        value4
+    """
+    if target_key in dictionary:
+        return dictionary[target_key]
+    
+    for value in dictionary.values():
+        if isinstance(value, dict):
+            result = find_key(value, target_key)
+            if result is not None:
+                return result
+    
+    return None
+
+find_key(experimental, 'n')
+
+def find_all_instances(dictionary, target_key1, target_key2, results_list):
+    """
+    Recursively finds instances of two target keys in a nested dictionary and appends their corresponding values together.
+
+    Parameters:
+        dictionary (dict): The nested dictionary to search.
+        target_key1 (hashable): The first target key to find.
+        target_key2 (hashable): The second target key to find.
+        results_list (list): The list where the corresponding values will be appended.
+
+    Returns:
+        None
+
+    Example:
+        >>> my_dict = {
+        ...     "a": 1,
+        ...     "b": {"c": 2, "d": 3},
+        ...     "e": {"f": 4, "g": {"a": 5, "c": 6}},
+        ...     "i": 7
+        ... }
+        >>> target_key1 = "a"
+        >>> target_key2 = "c"
+        >>> instances = []
+        >>> find_all_instances(my_dict, target_key1, target_key2, instances)
+        >>> print(instances)
+        [5, 6]
+    """
+    found_keys = set()
+    for key, value in dictionary.items():
+        if key == target_key1 or key == target_key2:
+            found_keys.add(key)
+        elif isinstance(value, dict):
+            find_all_instances(value, target_key1, target_key2, results_list)
+
+    if {target_key1, target_key2}.issubset(found_keys):
+        results_list.append(np.array([dictionary[target_key1], dictionary[target_key2]]).T)
